08a35041f0dab921553f8d93e4de718ff5b5ff97
Avidu <avidu97@gmail.com>
2022-02-14 20:31:37 +0530

    sanity installed

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { sanityClient } from '../sanity'
 
-export default function Home() {
+import React from 'react'
+
+const Home = () => {
   return (
-    <div className={styles.container}>
-      <h1><b>Hellooooo</b></h1>
-    </div>
+    <div>index</div>
   )
 }
+
+export default Home
